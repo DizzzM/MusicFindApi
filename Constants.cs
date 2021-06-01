452c7c6cd6c2a61589184ec7fae145ed4caf77f1
_d1mo0on_ <76074137+DizzzM@users.noreply.github.com>
2021-06-02 00:04:02 +0300

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicFindAPI
+{
+    public static class Constants
+    {
+        public static readonly string SpotifyToken = "MDhmMDlmYzk3ZTE2NDk0NDk3MzkyNjc1NzkwNDk2YmM6OTZiZDkyMTU2MzJlNGY5OTg5ZDZjYzQ3Y2RiOTE2N2E==";
+
+        public static readonly string GeniusBearer = "CbvLfiE97zwDLJIY8gAmvK1xi364IXXl1OWCV6_7I1ix8c3mO51nfZeBm_LmJdLh";
+
+        public static readonly string MusixmatchKey = "40779daf796d4e24f37e9a852c21a83c";
+
+        public static readonly string FirebaseAuthSecret = "61YYdBbuAXsEgqcEihkTZ8WIlOH3u3cpM5HprQpy";
+        public static readonly string FirebaseBasePath = "https://musicfinderapi-default-rtdb.firebaseio.com/";
+    }
+}
